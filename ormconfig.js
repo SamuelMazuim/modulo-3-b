3ddfbe614d0a8c371f3ac3eca44ff00ca1e0bdad
SamuelMazuim <samuelmazuim@hotmail.com>
2022-03-02 21:37:02 -0300

    Update orm.config

--- ormconfig.js
+++ ormconfig.js
@@ -3,11 +3,11 @@ require("dotenv/config");
 module.exports = {
     type: "postgres",
     url: process.env.DATABASE_URL,
-    entities: ["src/core/infra/database/entities/**/*.ts"],
-    migrations: ["src/core/infra/database/migrations/**/*.ts"],
+    entities: ["**/core/infra/database/entities/**/*.{js, ts}"],
+    migrations: ["**/core/infra/database/migrations/**/*.{js, ts}"],
     cli: {
-        entitiesDir: "src/core/infra/database/entities",
-        migrationsDir: "src/core/infra/database/migrations",
+        entitiesDir: "**/core/infra/database/entities",
+        migrationsDir: "**/core/infra/database/migrations",
     },
     synchronize: false,
     extra: {
